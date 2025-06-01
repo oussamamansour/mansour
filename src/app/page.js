52c1ef11b05d3c88ed6b13afdad55bc7589b4d9a
mansour <37974663+oussamamansour@users.noreply.github.com>
2025-06-01 12:20:06 +0200

    personal website

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,20 @@
+'use client';
+import { useState } from "react";
+
 export default function Home() {
+const [copied, setCopied] = useState(false);
+  const copyToClipboard = async () => {
+    await navigator.clipboard.writeText("contact@oussamamansour.de")
+    setCopied(true);
+    setTimeout(() => setCopied(false), 4000);}
   return (
-    <main className="max-w-[1280px] mx-auto mt-8 px-8 font-mono">
-      <div className="flex gap-2.5 justify-left">
-        <span className="text-zinc-500 select-none">Email:</span>
+    <main className="max-w-[1280px] mx-auto mt-8 px-8 font-mono select-none">
+      <div className="flex gap-2.5 justify-left items-center">
+        <span className="text-zinc-500">Email:</span>
         <a href="mailto:contact@oussamamansour.de" className="">contact@oussamamansour.de</a>
+        <button
+          onClick={copyToClipboard}
+          className="bg-white text-black px-2 py-1 rounded-sm cursor-pointer">{copied ? 'Copied!' : 'Copy'}</button>
       </div>
     </main>
   );
